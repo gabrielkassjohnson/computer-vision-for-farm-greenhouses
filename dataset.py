d042a3c6890317637bb349b9a77c4f15b1790140
gabrielkassjohnson <aza13572468@gmail.com>
2022-07-23 12:30:46 -0400

    got model working with 2 categories, custom dataset

--- dataset.py
+++ dataset.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
-from torchvision.io import read_image
+from torchvision.io import read_image, ImageReadMode
+import torch.utils.data as data
+import torch
+from PIL import Image
 
 class PlantNetDataset:
     def __init__(self, img_dir, transforms):
@@ -16,14 +19,16 @@ class PlantNetDataset:
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, "images", self.imgs[idx])
         mask_path = os.path.join(self.img_dir, "masks", self.masks[idx])
-        img = Image.open(img_path).convert("RGB")
+        #img = read_image(img_path,mode=ImageReadMode.RGB)
+        img = Image.open(img_path).convert('RGB')
         # note that we haven't converted the mask to RGB,
         # because each color corresponds to a different instance
         # with 0 being background
+        #target = read_image(mask_path)
         target = Image.open(mask_path)
 
-        target = np.array(mask)
+        #target = np.array(mask)
         if self.transforms is not None:
             img, target = self.transforms(img, target)
 
-        return image, target
+        return img, target
